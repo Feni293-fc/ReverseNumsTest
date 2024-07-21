97558fbc1a9980cf270eb927dcdb0edf7434de3a
Luis Fernando Camargo Pineda <ferna293@hotmail.com>
2024-07-21 11:03:26 -0500

    Add project files.

---
+++ ReverseNums/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReverseNums;
+
+Console.WriteLine("Hello, World!");
+
+int[] arr = { 1, 2, 3, 4};
+int m = 1;
+
+Console.WriteLine("arr original");
+Rotate.PrintArray(arr);
+
+Rotate.Right(arr, m);
+
+Console.WriteLine($"arr rotado a la derecha {m} veses");
+Rotate.PrintArray(arr);
---
+++ ReverseNums/Rotate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNums
+{
+    public class Rotate
+    {
+        public static void Right(int[] arr, int start)
+        {
+            if (arr == null || arr.Length == 0)
+                return;
+
+            int end = arr.Length;
+            start %= end;
+
+            if (start == 0)
+                return;
+
+            ReverseNums(arr, 0, end - 1);
+            ReverseNums(arr, 0, start - 1);
+            ReverseNums(arr, start, end - 1);
+        }
+
+        public static void ReverseNums(int[] arr, int start, int end)
+        {
+            while (start < end)
+            {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+
+                start++;
+                end--;
+            }
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
